5228961c999373090c5fa864d6c54035903c963a
Peter537 <pean@outlook.dk>
2023-04-28 17:07:24 +0200

    refactored startcode

--- src/main/java/dat/backend/model/persistence/ConnectionPool.java
+++ src/main/java/dat/backend/model/persistence/ConnectionPool.java
@@ -8,38 +8,33 @@ import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-public class ConnectionPool
-{
-    // TODO: Change access credentials for MySql server as needed below:
+public class ConnectionPool {
 
-    private HikariDataSource ds;
-    private static String USER = "root";
-    private static String PASSWORD = "root";
-    private static String URL = "jdbc:mysql://localhost:3306/startcode";
+    private final HikariDataSource ds;
+    private static final String USER = "root";
+    private static final String PASSWORD = "123";
+    private static final String URL = "jdbc:mysql://localhost:3306/fogprojekt";
 
-    public ConnectionPool()
-    {
+    public ConnectionPool() {
         this(USER, PASSWORD, URL);
     }
 
-    public ConnectionPool(String USER, String PASSWORD, String URL)
-    {
+    public ConnectionPool(String user, String password, String url) {
         String deployed = System.getenv("DEPLOYED");
-        if (deployed != null)
-        {
+        if (deployed != null) {
             // Prod: hent variabler fra setenv.sh i Tomcats bin folder
-            USER = System.getenv("JDBC_USER");
-            PASSWORD = System.getenv("JDBC_PASSWORD");
-            URL = System.getenv("JDBC_CONNECTION_STRING");
+            user = System.getenv("JDBC_USER");
+            password = System.getenv("JDBC_PASSWORD");
+            url = System.getenv("JDBC_CONNECTION_STRING");
         }
 
         Logger.getLogger("web").log(Level.INFO,
-                String.format("Connection Pool created for: (%s, %s, %s)", USER, PASSWORD, URL));
+                String.format("Connection Pool created for: (%s, %s, %s)", user, password, url));
         HikariConfig config = new HikariConfig();
         config.setDriverClassName("com.mysql.cj.jdbc.Driver");
-        config.setJdbcUrl(URL);
-        config.setUsername(USER);
-        config.setPassword(PASSWORD);
+        config.setJdbcUrl(url);
+        config.setUsername(user);
+        config.setPassword(password);
         config.setMaximumPoolSize(5);
         config.addDataSourceProperty("cachePrepStmts", "true");
         config.addDataSourceProperty("prepStmtCacheSize", "250");
@@ -47,16 +42,13 @@ public class ConnectionPool
         this.ds = new HikariDataSource(config);
     }
 
-    public Connection getConnection() throws SQLException
-    {
+    public Connection getConnection() throws SQLException {
         Logger.getLogger("web").log(Level.INFO, ": get data connection");
         return ds.getConnection();
     }
 
-    public void close()
-    {
+    public void close() {
         Logger.getLogger("web").log(Level.INFO, "Shutting down connection pool");
         ds.close();
     }
-
 }
