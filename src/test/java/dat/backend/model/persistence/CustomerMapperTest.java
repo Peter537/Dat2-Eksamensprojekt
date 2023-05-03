d2b0522077ca15db89da61b52996b827c84dc750
Peter537 <52176852+Peter537@users.noreply.github.com>
2023-05-03 12:35:05 +0200

    created new test classes and removed startcode test

---
+++ src/test/java/dat/backend/model/persistence/CustomerMapperTest.java
@@ -0,0 +1,66 @@
+package dat.backend.model.persistence;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CustomerMapperTest {
+
+    private final static String USER = "root";
+    private final static String PASSWORD = "123";
+    private final static String URL = "jdbc:mysql://localhost:3306/fogcarport_test?serverTimezone=CET&allowPublicKeyRetrieval=true&useSSL=false";
+
+    private static ConnectionPool connectionPool;
+
+    @BeforeAll
+    public static void setUpClass() {
+        connectionPool = new ConnectionPool(USER, PASSWORD, URL);
+
+        try (Connection testConnection = connectionPool.getConnection()) {
+            try (Statement stmt = testConnection.createStatement()) {
+                // Create test database - if not exist
+                stmt.execute("CREATE DATABASE IF NOT EXISTS fogcarport_test;");
+
+                // TODO: Create user table. Add your own tables here
+                stmt.execute("CREATE TABLE IF NOT EXISTS fogcarport_test.customer LIKE fogcarport.customer;");
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+            fail("Database connection failed");
+        }
+    }
+
+    @BeforeEach
+    void setUp() {
+        try (Connection testConnection = connectionPool.getConnection()) {
+            try (Statement stmt = testConnection.createStatement()) {
+                // TODO: Remove all rows from all tables - add your own tables here
+                stmt.execute("DELETE FROM customer");
+
+                // TODO: Insert a few users - insert rows into your own tables here
+                stmt.execute("INSERT INTO customer (email, password) " +
+                        "VALUES ('ben@gmail.com', '123'), ('allan@outlook.dk', '1234'), ('alex@hotmail.com', '12345')");
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+            fail("Database connection failed");
+        }
+    }
+
+    @Test
+    void testConnection() throws SQLException {
+        Connection connection = connectionPool.getConnection();
+        assertNotNull(connection);
+        assertTrue(connection.isValid(1));
+        assertFalse(connection.isClosed());
+        connection.close();
+        assertTrue(connection.isClosed());
+        assertFalse(connection.isValid(1));
+    }
+}
