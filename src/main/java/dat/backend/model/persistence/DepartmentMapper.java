2db0db328a47f31ad27fd1d17993745315fd302b
Peter537 <52176852+Peter537@users.noreply.github.com>
2023-05-04 11:46:16 +0200

    created helper method to create Department

--- src/main/java/dat/backend/model/persistence/DepartmentMapper.java
+++ src/main/java/dat/backend/model/persistence/DepartmentMapper.java
@@ -9,28 +9,38 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Optional;
 
 class DepartmentMapper {
 
-    static Department getDepartmentById(int id, ConnectionPool connectionPool) throws DatabaseException {
+    static Optional<Department> getDepartmentById(int id, ConnectionPool connectionPool) throws DatabaseException {
         String query = "SELECT * FROM department WHERE id = ?";
         try (Connection connection = connectionPool.getConnection()) {
             try (PreparedStatement statement = connection.prepareStatement(query)) {
                 statement.setInt(1, id);
                 ResultSet resultSet = statement.executeQuery();
-                if (!resultSet.next()) {
-                    throw new DatabaseException("Could not get department");
-                }
-
-                String departmentName = resultSet.getString("name");
-                int zipCode = resultSet.getInt("zipcode");
-                String address = resultSet.getString("address");
-                Zip zip = ZipMapper.getZipByZipCode(zipCode, connectionPool);
-                Address addressObject = new Address(address, zip);
-                return new Department(id, departmentName, addressObject);
+                return createDepartmentFromResultSet(resultSet, connectionPool);
             }
         } catch (SQLException e) {
             throw new DatabaseException(e, "Could not get department");
         }
+    }
+
+    private static Optional<Department> createDepartmentFromResultSet(ResultSet resultSet, ConnectionPool connectionPool) throws SQLException, DatabaseException {
+        if (!resultSet.next()) {
+            return Optional.empty();
+        }
+
+        int id = resultSet.getInt("id");
+        String departmentName = resultSet.getString("name");
+        int zipCode = resultSet.getInt("zipcode");
+        String address = resultSet.getString("address");
+        Optional<Zip> zip = ZipMapper.getZipByZipCode(zipCode, connectionPool);
+        if (!zip.isPresent()) {
+            throw new DatabaseException("Could not get zip");
+        }
+
+        Address addressObject = new Address(address, zip.get());
+        return Optional.of(new Department(id, departmentName, addressObject));
     }
 }
