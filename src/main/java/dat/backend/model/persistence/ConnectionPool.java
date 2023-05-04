43cfd341e4c27d06d51fcbf49fbbd7e6eec786c4
Peter537 <52176852+Peter537@users.noreply.github.com>
2023-05-04 11:49:59 +0200

    updated URL

--- src/main/java/dat/backend/model/persistence/ConnectionPool.java
+++ src/main/java/dat/backend/model/persistence/ConnectionPool.java
@@ -13,7 +13,7 @@ public class ConnectionPool {
     private final HikariDataSource ds;
     private static final String USER = "root";
     private static final String PASSWORD = "123";
-    private static final String URL = "jdbc:mysql://localhost:3306/fogprojekt";
+    private static final String URL = "jdbc:mysql://localhost:3306/fogcarport";
 
     public ConnectionPool() {
         this(USER, PASSWORD, URL);
