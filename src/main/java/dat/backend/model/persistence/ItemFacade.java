8072461476a468d3694c15d9e2d1df0d981a6a89
Skainsmate <113088906+Skainsmate@users.noreply.github.com>
2023-05-07 18:54:51 +0200

    Created getLumberByLumberType in ItemFace and ItemMapper. They are just empty for now, but I need them for the OrderList.

--- src/main/java/dat/backend/model/persistence/ItemFacade.java
+++ src/main/java/dat/backend/model/persistence/ItemFacade.java
@@ -1,8 +1,20 @@
 package dat.backend.model.persistence;
 
+import dat.backend.model.entities.Lumber;
+import dat.backend.model.entities.LumberType;
+import dat.backend.model.exceptions.DatabaseException;
+
+import java.util.List;
+
 public class ItemFacade {
 
     /*
      * TODO: Implement this class
      */
+
+
+
+    public static List<Lumber> getLumberByLumberType(LumberType LumberType, ConnectionPool connectionPool) throws DatabaseException {
+        return ItemMapper.getLumberByLumberType(LumberType, connectionPool);
+    }
 }
--- src/main/java/dat/backend/model/persistence/ItemMapper.java
+++ src/main/java/dat/backend/model/persistence/ItemMapper.java
@@ -1,8 +1,20 @@
 package dat.backend.model.persistence;
 
+import dat.backend.model.entities.Lumber;
+import dat.backend.model.entities.LumberType;
+
+import java.util.ArrayList;
+import java.util.List;
+
 class ItemMapper {
 
     /*
      * TODO: Implement this class
      */
+
+
+
+    static List<Lumber> getLumberByLumberType(LumberType lumberType, ConnectionPool connectionPool) {
+        return new ArrayList<>();
+    }
 }
