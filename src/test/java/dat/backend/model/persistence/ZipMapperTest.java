c09b57e40e6c0b66127a6fcb6ebc6393454b0d6d
Peter537 <52176852+Peter537@users.noreply.github.com>
2023-05-08 09:34:21 +0200

    made test classes not public

--- src/test/java/dat/backend/model/persistence/ZipMapperTest.java
+++ src/test/java/dat/backend/model/persistence/ZipMapperTest.java
@@ -14,7 +14,7 @@ import java.sql.Statement;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 
-public class ZipMapperTest {
+class ZipMapperTest {
 
     private final static String USER = "root";
     private final static String PASSWORD = "123";
