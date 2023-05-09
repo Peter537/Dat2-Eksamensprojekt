1d0aaa8bc2df12afba8ab39bcd6d232ba4404a87
MagKon <magnus.kongstad@gmail.com>
2023-05-09 15:05:05 +0200

    Finished todos in mapper

--- src/main/java/dat/backend/model/persistence/LumberMapper.java
+++ src/main/java/dat/backend/model/persistence/LumberMapper.java
@@ -3,7 +3,6 @@ package dat.backend.model.persistence;
 import dat.backend.model.entities.Lumber;
 import dat.backend.model.entities.LumberType;
 import dat.backend.model.exceptions.DatabaseException;
-import dat.backend.model.services.Validation;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -27,7 +26,7 @@ class LumberMapper {
                     throw new DatabaseException("Could not create lumber");
                 }
 
-                return Optional.empty(); // TODO: return lumber
+                return getLumberById(id, connectionPool);
             }
         } catch (SQLException e) {
             throw new DatabaseException(e, "Could not create lumber");
@@ -48,7 +47,6 @@ class LumberMapper {
                     throw new DatabaseException("Could not get lumber by id");
                 }
 
-                int lumberid = resultSet.getInt("id");
                 int length = resultSet.getInt("length");
 
                 Optional<LumberType> lumberTypeOptional = LumbertypeFacade.getLumbertypeByType(resultSet.getString("type"), connectionPool);
@@ -62,7 +60,7 @@ class LumberMapper {
 
                 int amount = resultSet.getInt("amount");
 
-                return Optional.of(new Lumber(lumberid, length, lumberType, amount));
+                return Optional.of(new Lumber(id, length, lumberType, amount));
             }
         } catch (SQLException e) {
             throw new DatabaseException(e, "Could not get lumber by id");
@@ -95,8 +93,38 @@ class LumberMapper {
         }
     }
 
-    public static Optional<Lumber> getLumberByLength(int length, ConnectionPool connectionPool) {
-        return Optional.empty(); // TODO: return lumber
+    public static Optional<Lumber> getLumberByLength(int length, ConnectionPool connectionPool) throws DatabaseException {
+
+        String query = "SELECT * FROM lumber WHERE length = ?";
+        try (Connection connection = connectionPool.getConnection())
+        {
+            try (PreparedStatement statement = connection.prepareStatement(query))
+            {
+                statement.setInt(1, length);
+
+                ResultSet resultSet = statement.executeQuery();
+
+                if (!resultSet.next()) {
+                    throw new DatabaseException("Could not get lumber by length");
+                }
+
+                int lumberid = resultSet.getInt("id");
+                int amount = resultSet.getInt("amount");
+
+                Optional<LumberType> lumberTypeOptional = LumbertypeFacade.getLumbertypeByType(resultSet.getString("type"), connectionPool);
+                LumberType lumberType;
+                if (lumberTypeOptional.isPresent()) {
+                    lumberType = lumberTypeOptional.get();
+                }
+                else {
+                    throw new DatabaseException("Could not get lumber by length");
+                }
+
+                return Optional.of(new Lumber(lumberid, length, lumberType, amount));
+            }
+        } catch (SQLException e) {
+            throw new DatabaseException(e, "Could not get lumber by length");
+        }
     }
 
 
