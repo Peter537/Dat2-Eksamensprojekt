efa3919678fecec6c12f9a0a2a65bcdd4c7bb80c
Peter537 <52176852+Peter537@users.noreply.github.com>
2023-05-09 15:11:46 +0200

    Create ChangeEmployeePhoneNumber.java

---
+++ src/main/java/dat/backend/control/ChangeEmployeePhoneNumber.java
@@ -0,0 +1,78 @@
+package dat.backend.control;
+
+import dat.backend.model.config.ApplicationStart;
+import dat.backend.model.entities.Employee;
+import dat.backend.model.exceptions.DatabaseException;
+import dat.backend.model.exceptions.ValidationException;
+import dat.backend.model.persistence.ConnectionPool;
+import dat.backend.model.persistence.EmployeeFacade;
+import dat.backend.model.services.Validation;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.util.Optional;
+
+@WebServlet(name = "change-employee-phonenumber", value = "/change-employee-phonenumber")
+public class ChangeEmployeePhoneNumber extends HttpServlet {
+
+    private ConnectionPool connectionPool;
+
+    @Override
+    public void init() throws ServletException {
+        this.connectionPool = ApplicationStart.getConnectionPool();
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        HttpSession session = request.getSession();
+        Employee employee = (Employee) session.getAttribute("employee");
+        String newPersonalPhoneNumber = request.getParameter("newPersonalPhoneNumber");
+        if (newPersonalPhoneNumber != null) {
+            if (employee.getPersonalPhoneNumber().isPresent()) {
+                if (employee.getPersonalPhoneNumber().get().equals(newPersonalPhoneNumber)) {
+                    return;
+                }
+            }
+
+            try {
+                Validation.validatePhoneNumber(newPersonalPhoneNumber);
+                EmployeeFacade.updatePersonalPhoneNumber(employee, newPersonalPhoneNumber, connectionPool);
+                request.getRequestDispatcher("WEB-INF/employeeOverview.jsp").forward(request, response);
+            } catch (ValidationException e) {
+                request.setAttribute("errormessage", "'" + newPersonalPhoneNumber + "' er ikke et dansk telefonnummer");
+                request.getRequestDispatcher("WEB-INF/employeeOverview.jsp").forward(request, response);
+            } catch (DatabaseException e) {
+                request.setAttribute("errormessage", e.getMessage());
+                request.getRequestDispatcher("WEB-INF/error.jsp").forward(request, response);
+                return;
+            }
+        }
+
+        String newWorkPhoneNumber = request.getParameter("newWorkPhoneNumber");
+        if (newWorkPhoneNumber != null) {
+            if (employee.getWorkPhoneNumber().isPresent()) {
+                if (employee.getWorkPhoneNumber().get().equals(newWorkPhoneNumber)) {
+                    return;
+                }
+            }
+
+            try {
+                Validation.validatePhoneNumber(newWorkPhoneNumber);
+                EmployeeFacade.updateWorkPhoneNumber(employee, newWorkPhoneNumber, connectionPool);
+                request.getRequestDispatcher("WEB-INF/employeeOverview.jsp").forward(request, response);
+            } catch (ValidationException e) {
+                request.setAttribute("errormessage", "'" + newWorkPhoneNumber + "' er ikke et dansk telefonnummer");
+                request.getRequestDispatcher("WEB-INF/employeeOverview.jsp").forward(request, response);
+            } catch (DatabaseException e) {
+                request.setAttribute("errormessage", e.getMessage());
+                request.getRequestDispatcher("WEB-INF/error.jsp").forward(request, response);
+                return;
+            }
+        }
+    }
+}
