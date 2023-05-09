f5da391f1010478034950f77c2f398319e98ba99
MagKon <magnus.kongstad@gmail.com>
2023-05-09 15:05:20 +0200

    Added get lumbertype by id

--- src/main/java/dat/backend/model/persistence/LumbertypeMapper.java
+++ src/main/java/dat/backend/model/persistence/LumbertypeMapper.java
@@ -2,12 +2,38 @@ package dat.backend.model.persistence;
 
 import dat.backend.model.entities.Lumber;
 import dat.backend.model.entities.LumberType;
+import dat.backend.model.exceptions.DatabaseException;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.Optional;
 
 public class LumbertypeMapper {
-    public static Optional<LumberType> getLumberByType(String lumberType, ConnectionPool connectionPool) {
+    public static Optional<LumberType> getLumbertypeByType(String lumberType, ConnectionPool connectionPool) throws DatabaseException {
 
-        return null; //TODO: Implement this method
+        String query = "SELECT * FROM lumbertype WHERE type = ?";
+        try (Connection connection = connectionPool.getConnection()) {
+            try (PreparedStatement statement = connection.prepareStatement(query)) {
+
+                    statement.setString(1, lumberType);
+
+                    ResultSet resultSet = statement.executeQuery();
+
+                    if (!resultSet.next()) {
+                        throw new DatabaseException("Could not get lumber by id");
+                    }
+
+                    int id = resultSet.getInt("id");
+                    float thickness = resultSet.getFloat("thickness");
+                    float width = resultSet.getFloat("width");
+                    float price = resultSet.getFloat("price");
+
+                    return Optional.of(new LumberType(id, thickness, width, price, lumberType));
+            }
+        } catch (SQLException e) {
+            throw new DatabaseException(e, "Could not get lumber by id");
+        }
     }
 }
