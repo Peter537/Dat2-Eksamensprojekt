f4fb8e3523af8fcf411fd5a29f2257317b25f0c7
Peter537 <52176852+Peter537@users.noreply.github.com>
2023-05-10 12:15:54 +0200

    Changed OrderStatus to use generic exception

--- src/main/java/dat/backend/model/persistence/order/OrderStatusFacade.java
+++ src/main/java/dat/backend/model/persistence/order/OrderStatusFacade.java
@@ -2,12 +2,12 @@ package dat.backend.model.persistence.order;
 
 import dat.backend.model.entities.OrderStatus;
 import dat.backend.model.exceptions.DatabaseException;
-import dat.backend.model.exceptions.OrderStatusNotFoundException;
+import dat.backend.model.exceptions.NotFoundException;
 import dat.backend.model.persistence.ConnectionPool;
 
 public class OrderStatusFacade {
 
-    public static OrderStatus getOrderStatusByStatus(String status, ConnectionPool connectionPool) throws OrderStatusNotFoundException, DatabaseException {
+    public static OrderStatus getOrderStatusByStatus(String status, ConnectionPool connectionPool) throws NotFoundException, DatabaseException {
         return OrderStatusMapper.getOrderStatusByStatus(status, connectionPool);
     }
 }
