bb3c3159bdebbdde7ddbbc50a19c2662bdbbbc5a
MagKon <magnus.kongstad@gmail.com>
2023-05-10 14:07:51 +0200

    Added wait function and reset of cp

--- src/test/java/dat/backend/model/persistence/TestDatabase.java
+++ src/test/java/dat/backend/model/persistence/TestDatabase.java
@@ -17,6 +17,13 @@ public abstract class TestDatabase {
     public abstract void setUp();
     @Test
     void testConnection() throws SQLException {
+        connectionPool.close();
+        connectionPool = new ConnectionPool(System.getenv("JDBC_USER"), System.getenv("JDBC_PASSWORD"), System.getenv("JDBC_CONNECTION_STRING"));
+        long time = System.currentTimeMillis();
+        long waitto = time + 5000;
+        while (time < waitto) {
+            time = System.currentTimeMillis();
+        }
         Connection connection = connectionPool.getConnection();
         assertNotNull(connection);
         assertTrue(connection.isValid(1));
