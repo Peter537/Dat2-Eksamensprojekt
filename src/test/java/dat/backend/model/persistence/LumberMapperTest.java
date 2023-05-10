95bdf6ff7c434b94fc661753286bb444ab2093ad
MagKon <magnus.kongstad@gmail.com>
2023-05-10 10:10:46 +0200

    First test made

--- src/test/java/dat/backend/model/persistence/LumberMapperTest.java
+++ src/test/java/dat/backend/model/persistence/LumberMapperTest.java
@@ -1,4 +1,100 @@
 package dat.backend.model.persistence;
 
+import dat.backend.model.entities.Lumber;
+import dat.backend.model.entities.LumberType;
+import dat.backend.model.exceptions.DatabaseException;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
 public class LumberMapperTest {
+    private final static String USER = "root";
+    private final static String PASSWORD = "123";
+    private final static String URL = "jdbc:mysql://localhost:3306/fogcarport_test?serverTimezone=CET&allowPublicKeyRetrieval=true&useSSL=false";
+
+    private static ConnectionPool connectionPool;
+
+    @BeforeAll
+    public static void setUpClass() {
+        connectionPool = new ConnectionPool(System.getenv("JDBC_USER"), System.getenv("JDBC_PASSWORD"), System.getenv("JDBC_CONNECTION_STRING"));
+
+        try (Connection testConnection = connectionPool.getConnection()) {
+            try (Statement stmt = testConnection.createStatement()) {
+                // Create test database - if not exist
+                stmt.execute("CREATE DATABASE IF NOT EXISTS fogcarport_test;");
+
+                // TODO: Create user table. Add your own tables here
+                stmt.execute("CREATE TABLE IF NOT EXISTS fogcarport_test.lumber LIKE fogcarport.lumber;");
+                stmt.execute("CREATE TABLE IF NOT EXISTS fogcarport_test.lumbertype LIKE fogcarport.lumbertype;");
+                stmt.execute("CREATE TABLE IF NOT EXISTS fogcarport_test.type LIKE fogcarport.type;");
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+            fail("Database connection failed");
+        }
+    }
+
+    @BeforeEach
+    void setUp() {
+        try (Connection testConnection = connectionPool.getConnection()) {
+            try (Statement stmt = testConnection.createStatement()) {
+                // TODO: Remove all rows from all tables - add your own tables here
+                stmt.execute("DELETE FROM lumber");
+                stmt.execute("DELETE FROM lumbertype");
+                stmt.execute("DELETE FROM type");
+                stmt.execute("ALTER TABLE lumber AUTO_INCREMENT = 1;");
+                stmt.execute("ALTER TABLE lumbertype AUTO_INCREMENT = 1;");
+
+                // TODO: Insert a few lumbers - insert rows into your own tables here
+                stmt.execute("INSERT INTO type (type, displayname) " +
+                        "VALUES ('RAFTER', 'Spærtræ'), ('POLE', 'Stolpe'), ('PLASTIC_ROOF', 'Plastic tag')");
+                stmt.execute("INSERT INTO lumbertype (thickness, width, type, meter_price) " +
+                        "VALUES (97, 97, 'POLE', 60), (45, 195, 'RAFTER', 48), (45, 245, 'RAFTER', 82)");
+                stmt.execute("INSERT INTO lumber (length, type, amount)" +
+                        " VALUES (180, 1, 1000), (240, 1, 1000), (360, 2, 1000)");
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+            fail("Database connection failed");
+        }
+    }
+
+    @Test
+    void testConnection() throws SQLException {
+        Connection connection = connectionPool.getConnection();
+        assertNotNull(connection);
+        assertTrue(connection.isValid(1));
+        assertFalse(connection.isClosed());
+        connection.close();
+        assertTrue(connection.isClosed());
+        assertFalse(connection.isValid(1));
+    }
+
+    @Test
+    void testValidGetLumberById() throws DatabaseException {
+        // Arrange
+        int id = 1;
+        int expectedLength = 180;
+        LumberType expectedType = LumbertypeMapper.getLumbertypeById(1, connectionPool).orElse(null);
+        int expectedAmount = 1000;
+        assert expectedType != null;
+        int expectedPrice = Math.round(expectedType.getMeterPrice() * expectedLength);
+
+        // Act
+        Lumber lumber = LumberMapper.getLumberById(id, connectionPool).orElse(null);
+
+        // Assert
+        assertNotNull(lumber);
+        assertEquals(expectedLength, lumber.getLength());
+        assertEquals(expectedType, lumber.getLumberType());
+        assertEquals(expectedPrice, lumber.getPrice());
+        assertEquals(expectedAmount, lumber.getAmount());
+    }
 }
