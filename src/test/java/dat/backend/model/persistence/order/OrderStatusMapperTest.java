6a46cb9aad9939d72b698aaee5396ce06bd94685
Peter537 <52176852+Peter537@users.noreply.github.com>
2023-05-12 12:09:45 +0200

    made class package private

--- src/test/java/dat/backend/model/persistence/order/OrderStatusMapperTest.java
+++ src/test/java/dat/backend/model/persistence/order/OrderStatusMapperTest.java
@@ -13,7 +13,7 @@ import java.sql.Statement;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-public class OrderStatusMapperTest extends TestDatabase {
+class OrderStatusMapperTest extends TestDatabase {
 
     @BeforeEach
     public void setUp() {
