031893690487fed32ae6ebb82a5fe32839228b49
Skainsmate <113088906+Skainsmate@users.noreply.github.com>
2023-05-15 10:41:26 +0200

    Refactored partslist to check for max length of lumber in database.

--- src/main/java/dat/backend/model/print3d/Plate3D.java
+++ src/main/java/dat/backend/model/print3d/Plate3D.java
@@ -2,6 +2,7 @@ package dat.backend.model.print3d;
 
 import dat.backend.model.entities.PartsList;
 import dat.backend.model.entities.item.LumberType;
+import dat.backend.model.exceptions.DatabaseException;
 import org.abstractica.javacsg.*;
 
 public class Plate3D {
@@ -9,12 +10,12 @@ public class Plate3D {
 
     PartsList partsList;
 
-    public Plate3D(PartsList partsList) {
+    public Plate3D(PartsList partsList) throws DatabaseException {
         this.plate = createPLate(partsList);
         this.partsList = partsList;
     }
 
-    private Geometry3D createPLate(PartsList partsList) {
+    private Geometry3D createPLate(PartsList partsList) throws DatabaseException {
         JavaCSG csg = JavaCSGFactory.createDefault();
         LumberType plate = partsList.getPlate().getLumberType();
         Geometry3D box = csg.box3D( plate.getWidth(), plate.getThickness(), partsList.getLengthOfPlate(), true);
--- src/main/java/dat/backend/model/print3d/Rafter3D.java
+++ src/main/java/dat/backend/model/print3d/Rafter3D.java
@@ -2,6 +2,7 @@ package dat.backend.model.print3d;
 
 import dat.backend.model.entities.PartsList;
 import dat.backend.model.entities.item.LumberType;
+import dat.backend.model.exceptions.DatabaseException;
 import org.abstractica.javacsg.*;
 
 public class Rafter3D {
@@ -9,12 +10,12 @@ public class Rafter3D {
 
     PartsList partsList;
 
-    public Rafter3D(PartsList partsList) {
+    public Rafter3D(PartsList partsList) throws DatabaseException {
         this.rafter = createRafter(partsList);
         this.partsList = partsList;
     }
 
-    private Geometry3D createRafter(PartsList partsList) {
+    private Geometry3D createRafter(PartsList partsList) throws DatabaseException {
         JavaCSG csg = JavaCSGFactory.createDefault();
         LumberType rafter = partsList.getRafter().getLumberType();
         Geometry3D box = csg.box3D( rafter.getWidth(), rafter.getThickness(), partsList.getLengthOfRafter(), true);
