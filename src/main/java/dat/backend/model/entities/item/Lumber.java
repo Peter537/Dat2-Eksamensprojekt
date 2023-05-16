fa92921b25e5f7f203d786249256ae8f99e09cb0
Peter537 <52176852+Peter537@users.noreply.github.com>
2023-05-16 11:06:01 +0200

    Removed Item abstract class

--- src/main/java/dat/backend/model/entities/item/Lumber.java
+++ src/main/java/dat/backend/model/entities/item/Lumber.java
@@ -4,19 +4,30 @@ import dat.backend.annotation.IgnoreCoverage;
 
 import java.util.Objects;
 
-public class Lumber extends Item implements Comparable<Lumber> {
+public class Lumber implements Comparable<Lumber> {
 
+    private int id;
     private int length;
     private LumberType lumberType;
     private int price;
     private int amount;
 
     public Lumber(int id, int length, LumberType lumberType, int price, int amount) {
-        super(id);
+        this.id = id;
         this.length = length;
         this.lumberType = lumberType;
         this.price = price;
         this.amount = amount;
+    }
+
+    @IgnoreCoverage(reason = "Getter or Setter")
+    public int getId() {
+        return this.id;
+    }
+
+    @IgnoreCoverage(reason = "Getter or Setter")
+    public void setId(int id) {
+        this.id = id;
     }
 
     @IgnoreCoverage(reason = "Getter or Setter")
@@ -65,7 +76,7 @@ public class Lumber extends Item implements Comparable<Lumber> {
         if (this == other) return true;
         if (!(other instanceof Lumber)) return false;
         Lumber lumber = (Lumber) other;
-        return super.equals(other) &&
+        return this.getId() == lumber.getId() &&
                 this.getLength() == lumber.getLength() &&
                 this.getLumberType().equals(lumber.getLumberType());
     }
@@ -73,14 +84,14 @@ public class Lumber extends Item implements Comparable<Lumber> {
     @IgnoreCoverage(reason = "hashCode")
     @Override
     public int hashCode() {
-        return Objects.hash(super.hashCode(), this.getLength(), this.getLumberType(), this.getAmount(), this.getPrice());
+        return Objects.hash(this.getId(), this.getLength(), this.getLumberType(), this.getAmount(), this.getPrice());
     }
 
     @IgnoreCoverage(reason = "toString")
     @Override
     public String toString() {
         return "Lumber{" +
-                "id=" + this.getId() +
+                "id=" + this.id +
                 ", length=" + this.length +
                 ", lumberType=" + this.lumberType +
                 ", amount=" + this.amount +
