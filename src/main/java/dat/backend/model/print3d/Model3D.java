53000e6762d9938f39bc7f2b29e1a7d01d1f0b85
Skainsmate <113088906+Skainsmate@users.noreply.github.com>
2023-05-16 09:10:22 +0200

    Added 3Dtest

--- src/main/java/dat/backend/model/print3d/Model3D.java
+++ src/main/java/dat/backend/model/print3d/Model3D.java
@@ -1,11 +1,14 @@
 package dat.backend.model.print3d;
 
 import dat.backend.model.entities.PartsList;
+import dat.backend.model.exceptions.DatabaseException;
 
 public class Model3D {
 
 
-    void createModel(PartsList partsList) {
-
+    void createModel(PartsList partsList) throws DatabaseException {
+        Pole3D pole3D = new Pole3D(partsList);
+        Plate3D plate3D = new Plate3D(partsList);
+        Rafter3D rafter3D = new Rafter3D(partsList);
     }
 }
--- src/test/java/dat/backend/model/print3d/Model3DTest.java
+++ src/test/java/dat/backend/model/print3d/Model3DTest.java
@@ -15,17 +15,20 @@ import java.sql.Statement;
 import static org.junit.jupiter.api.Assertions.*;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
-class Pole3DTest extends TestDatabase {
+class Model3DTest extends TestDatabase {
     @BeforeEach
     public void setUp() {
         try (Connection testConnection = connectionPool.getConnection()) {
             try (Statement stmt = testConnection.createStatement()) {
                 // Remove all rows from all tables - add your own tables here
+                stmt.execute("DELETE FROM roof");
                 stmt.execute("DELETE FROM lumber");
                 stmt.execute("DELETE FROM lumbertype");
                 stmt.execute("DELETE FROM type");
                 stmt.execute("ALTER TABLE lumber AUTO_INCREMENT = 1;");
                 stmt.execute("ALTER TABLE lumbertype AUTO_INCREMENT = 1;");
+                stmt.execute("ALTER TABLE roof AUTO_INCREMENT = 1;");
+
 
                 // Insert a few lumbers - insert rows into your own tables here
                 stmt.execute("INSERT INTO type (type, displayname) " +
@@ -33,15 +36,15 @@ class Pole3DTest extends TestDatabase {
                 stmt.execute("INSERT INTO lumbertype (thickness, width, type, meter_price) " +
                         "VALUES (97, 97, 'POLE', 60), (45, 195, 'RAFTER', 48), (45, 245, 'RAFTER', 82)");
                 stmt.execute("INSERT INTO lumber (length, type, amount)" +
-                        " VALUES (300, 1, 1000), (480, 1, 1000), (360, 2, 1000), (360, 2, 1000), (720,2,1000), (720, 3, 1000)");
+                        " VALUES (300, 1, 1000), (480, 1, 1000), (360, 2, 1000), (360, 2, 1000), (720,2,1000), (800,2,1000), (720, 3, 1000)");
+                stmt.execute("INSERT INTO roof (squaremeter_price, type) " +
+                        "VALUES (100, 'PLASTIC_ROOF'), (200, 'TILED_ROOF')");
+
             }
-        }
-        catch (SQLException e) {
+        } catch (SQLException e) {
             System.out.println(e.getMessage());
             fail("Database connection failed");
         }
-
-
     }
 
     @Test
@@ -53,10 +56,10 @@ class Pole3DTest extends TestDatabase {
     }
 
     @Test
-    void ValidCreateCutout() throws DatabaseException, NotFoundException {
+    void ValidModel() throws DatabaseException, NotFoundException {
         PartsList partsList = new PartsList(200,500,300,super.connectionPool);
-        Pole3D pole3D = new Pole3D(partsList);
-        assertNotNull(pole3D);
+        Model3D model3D = new Model3D();
+        assertNotNull(model3D);
 
     }
 }
