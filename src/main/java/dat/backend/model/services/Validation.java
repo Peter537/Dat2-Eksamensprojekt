c62aef93b7e646ed04cacf455dbb307cab5d0762
Peter537 <52176852+Peter537@users.noreply.github.com>
2023-05-17 10:39:39 +0200

    Added validateThickness method, and updated validatePrice

--- src/main/java/dat/backend/model/services/Validation.java
+++ src/main/java/dat/backend/model/services/Validation.java
@@ -197,6 +197,10 @@ public class Validation {
         if (aFloat == null) {
             throw new ValidationException("Invalid price");
         }
+
+        if (aFloat < 0) {
+            throw new ValidationException("Invalid price");
+        }
     }
 
     public static void validateRemarks(String s) throws ValidationException {
@@ -220,6 +224,12 @@ public class Validation {
     public static void validateAmount(int amount) throws ValidationException {
         if (amount <= 0) {
             throw new ValidationException("Invalid amount");
+        }
+    }
+
+    public static void validateThickness(float thickness) throws ValidationException {
+        if (thickness <= 0) {
+            throw new ValidationException("Invalid thickness");
         }
     }
 }
