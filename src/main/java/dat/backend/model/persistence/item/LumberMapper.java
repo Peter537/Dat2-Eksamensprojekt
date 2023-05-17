0af2818f9c7ebd272e8a8ae192a7bedc823faf01
Peter537 <52176852+Peter537@users.noreply.github.com>
2023-05-17 10:30:04 +0200

    Added Validation and small other changes

--- src/main/java/dat/backend/model/persistence/item/LumberMapper.java
+++ src/main/java/dat/backend/model/persistence/item/LumberMapper.java
@@ -4,23 +4,21 @@ import dat.backend.model.entities.item.Lumber;
 import dat.backend.model.entities.item.LumberType;
 import dat.backend.model.exceptions.DatabaseException;
 import dat.backend.model.exceptions.NotFoundException;
+import dat.backend.model.exceptions.ValidationException;
 import dat.backend.model.persistence.ConnectionPool;
+import dat.backend.model.services.Validation;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
-import static java.sql.Statement.RETURN_GENERATED_KEYS;
-
 class LumberMapper {
 
-    static Lumber createLumber(int length, int type, int amount, ConnectionPool connectionPool) throws DatabaseException {
+    static Lumber createLumber(int length, int type, int amount, ConnectionPool connectionPool) throws DatabaseException, ValidationException {
+        Validation.validateLength(length);
         String query = "INSERT INTO lumber (length, type, amount) VALUES (?, ?, ?)";
         try (Connection connection = connectionPool.getConnection()) {
-            try (PreparedStatement statement = connection.prepareStatement(query, RETURN_GENERATED_KEYS)) {
+            try (PreparedStatement statement = connection.prepareStatement(query, Statement.RETURN_GENERATED_KEYS)) {
                 statement.setFloat(1, length);
                 statement.setInt(2, type);
                 statement.setInt(3, amount);
@@ -64,23 +62,28 @@ class LumberMapper {
         return lumberList;
     }
 
-    static void deleteLumber(int id, ConnectionPool connectionPool) throws DatabaseException {
+    static void deleteLumber(int id, ConnectionPool connectionPool) throws DatabaseException, NotFoundException {
         String query = "DELETE FROM lumber WHERE id = ?";
         try (Connection connection = connectionPool.getConnection()) {
             try (PreparedStatement statement = connection.prepareStatement(query)) {
                 statement.setInt(1, id);
-                statement.executeUpdate();
+                int rowsAffected = statement.executeUpdate();
+                if (rowsAffected != 1) {
+                    throw new NotFoundException("Could not delete lumber");
+                }
             }
         } catch (SQLException e) {
             throw new DatabaseException(e, "Could not delete lumber");
         }
     }
 
-    public static void updateLumber(int id, float poleLength, int lumberTypeId, int amount, ConnectionPool connectionPool) throws DatabaseException {
+    static void updateLumber(int id, int poleLength, int lumberTypeId, int amount, ConnectionPool connectionPool) throws DatabaseException, ValidationException {
+        Validation.validateLength(poleLength);
+        Validation.validateAmount(amount);
         String query = "UPDATE lumber SET length = ?, type = ?, amount = ? WHERE id = ?";
         try (Connection connection = connectionPool.getConnection()) {
             try (PreparedStatement statement = connection.prepareStatement(query)) {
-                statement.setFloat(1, poleLength);
+                statement.setInt(1, poleLength);
                 statement.setInt(2, lumberTypeId);
                 statement.setInt(3, amount);
                 statement.setInt(4, id);
