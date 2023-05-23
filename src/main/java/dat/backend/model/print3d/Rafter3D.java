794468a3f398f2fc5f30af35db43b9b54fb03cd6
Skainsmate <113088906+Skainsmate@users.noreply.github.com>
2023-05-23 20:19:22 +0200

    Rafter3D ændret til at lave små tapper på hver ende af spærene.

--- src/main/java/dat/backend/model/print3d/Rafter3D.java
+++ src/main/java/dat/backend/model/print3d/Rafter3D.java
@@ -12,25 +12,44 @@ public class Rafter3D {
     private final JavaCSG csg;
 
     private Geometry3D rafter;
-    private int numberOfRafters;
 
     public Rafter3D(JavaCSG csg, PartsList partsList) throws DatabaseException {
         this.csg = csg;
         this.partsList = partsList;
-        this.numberOfRafters = partsList.getNumberOfRafters();
         this.rafter = createRafter();
     }
 
     private Geometry3D createRafter() throws DatabaseException {
         LumberType rafterType = partsList.getRafter().getLumberType();
-        Geometry3D box = csg.box3D(partsList.getLengthOfRafter() * 10, rafterType.getThickness(), rafterType.getWidth(), false);
+        int numberOfRafters = partsList.getNumberOfRafters();
+        int length = partsList.getLengthOfRafter() * 10;
+        int width = (int)rafterType.getWidth();
+        int thickness = (int)rafterType.getThickness();
+
+        Geometry3D box = csg.box3D(length, width, thickness, false);
+
+        // Create cylinder for taps
+        Geometry3D hole = csg.cylinder3D((double) width * 0.4, (double) thickness / 2, 50, false);
+
+        // Position holes at each end of the rafter
+        Geometry3D tap1 = csg.translate3D((double) -(length - width) /2, 0, (double) thickness ).transform(hole);
+        Geometry3D tap2 = csg.translate3D((double) (length - width) /2, 0, (double) thickness).transform(hole);
+
+        // Subtract the holes from the box to create rafter with holes
+        box = csg.union3D(box, tap1);
+        box = csg.union3D(box, tap2);
+
         Geometry3D allRafters = box;
         for (int i = 1; i < numberOfRafters; i++) {
-            Geometry3D nextRafter = csg.translate3DY(rafterType.getThickness() * 1.5 * i).transform(box);
+            Geometry3D nextRafter = csg.translate3DY(width * 1.5 * i).transform(box);
             allRafters = csg.union3D(allRafters, nextRafter);
         }
 
         csg.view(allRafters, 3);
         return allRafters;
+    }
+
+    public Geometry3D getRafter() {
+        return this.rafter;
     }
 }
