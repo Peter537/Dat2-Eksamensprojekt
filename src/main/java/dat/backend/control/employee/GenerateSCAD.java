82cf9348d4a5683b393eb2bbc73165533cf66331
TheLonelyDude <magnus.kongstad@gmail.com>
2023-05-23 11:28:54 +0200

    Added output zip

--- src/main/java/dat/backend/control/employee/GenerateSCAD.java
+++ src/main/java/dat/backend/control/employee/GenerateSCAD.java
@@ -6,11 +6,16 @@ import dat.backend.model.exceptions.DatabaseException;
 import dat.backend.model.print3d.Model3D;
 
 import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
 @IgnoreCoverage(reason = "Servlet class should not be tested")
 @WebServlet(name = "GenerateSCAD", value = "/GenerateSCAD")
@@ -24,6 +29,41 @@ public class GenerateSCAD extends HttpServlet {
         } catch (DatabaseException e) {
             throw new RuntimeException(e);
         }
+
+        //Prepare package
+        String[] paths = {"/OpenSCAD/View1.scad", "/OpenSCAD/View2.scad", "/OpenSCAD/View3.scad"};
+        ServletOutputStream servletOutputStream = response.getOutputStream();
+        ZipOutputStream zipOutputStream = new ZipOutputStream(servletOutputStream);
+
+        //Set headers
+        response.setContentType("application/zip");
+        response.setHeader("Content-Disposition", "attachment; filename=\"package.zip\"");
+
+        //Iterate through files
+        for (String filePath : paths) {
+            File view = new File(filePath);
+            try (FileInputStream fileInputStream = new FileInputStream(view)) {
+                //Create zip entry
+                ZipEntry zipEntry = new ZipEntry(view.getName());
+                zipOutputStream.putNextEntry(zipEntry);
+
+                //Write file to output stream
+                byte[] bufferData = new byte[4096];
+                int bytesRead;
+                while ((bytesRead = fileInputStream.read(bufferData)) != -1) {
+                    zipOutputStream.write(bufferData, 0, bytesRead);
+                }
+
+                //Close entry
+                zipOutputStream.closeEntry();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        //Close streams
+        zipOutputStream.close();
+        servletOutputStream.close();
     }
 
     @Override
