19fd728ed3e963ec848747c04a4818597a1d6818
MagKon <magnus.kongstad@gmail.com>
2023-05-30 10:09:27 +0200

    Update profileSiteScript.js

--- src/main/webapp/scripts/profileSiteScript.js
+++ src/main/webapp/scripts/profileSiteScript.js
@@ -1,15 +1,15 @@
-let popup = document.getElementById("popup");
+let popup = document.getElementById("popup"); // Get the popup
 
 function openPopup() {
-    checkForPopup();
-    popup.classList.add("open-popup");
-    document.getElementById("baseBody").classList.add("after-popup");
+    checkForPopup(); // Check if popup is null (if it is, get it)
+    popup.classList.add("open-popup"); // Add the class to open the popup
+    document.getElementById("baseBody").classList.add("after-popup"); // Add the class to blur the background
 }
 
 function closePopup() {
-    checkForPopup();
-    popup.classList.remove("open-popup");
-    document.getElementById("baseBody").classList.remove("after-popup");
+    checkForPopup(); // Check if popup is null (if it is, get it)
+    popup.classList.remove("open-popup"); // Remove the class to open the popup
+    document.getElementById("baseBody").classList.remove("after-popup"); // Remove the class to blur the background
 }
 
 function checkForPopup() {
